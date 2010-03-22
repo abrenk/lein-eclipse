7b4671c411f8c1cfad83b00d54e3d05921d36cb4
Andreas Brenk <mail@andreasbrenk.com>
2010-03-22 17:25:51 +0100

    Create plugin stub

---
+++ src/leiningen/eclipse.clj
@@ -0,0 +1,8 @@
+(ns leiningen.eclipse
+  "Create Eclipse project descriptor files.")
+
+(defn eclipse
+  "Create Eclipse project descriptor files."
+  [project]
+  (println "Created .classpath")
+  (println "Created .project"))
